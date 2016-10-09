6da870714dd18ce0e8f77a68c23d42aef3261727
Abhiram4572 <subramanyam4572@gmail.com>
2016-10-10 01:04:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,8 +9,13 @@ element.innerHTML = 'new data written';
 //Move the image
 var img = document.getElementById("madi");
 
+function moveRight() {
+    marginLeft = marginLeft + 10 ;
+    img.style.marginLeft = marginLeft + 'px';
+}
+
 img.onclick = function() {
     
-    img.style.marginLeft ="100px";
+    var interval = setInterval(moveRight , 100);
     
 };
