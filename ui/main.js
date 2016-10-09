9c7331515dd5167aea99d86833da358f4b68e225
Abhiram4572 <subramanyam4572@gmail.com>
2016-10-10 02:01:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,21 @@
 
+//Move the image
+var img = document.getElementById("madi");
+
+var marginLeft = 0;
+
+function moveRight() {
+    marginLeft = marginLeft + 1 ;
+    img.style.marginLeft = marginLeft + 'px';
+}
+
+img.onclick = function() {
+    
+    var interval = setInterval(moveRight , 50);
+    
+};
+
+
 //Counter code
 var counter = 0;
 var button = document.getElementById("counter");
