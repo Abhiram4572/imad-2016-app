995439f0513c80c2866f6fef8e0c946e857a0f58
Abhiram4572 <subramanyam4572@gmail.com>
2016-10-10 01:40:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,6 +90,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0; 
+app.get('/counter' , function(req,res) {
+    counter = counter +1;
+    res.send(counter,toString());
+});
+
+
 app.get('/:articleName', function(req,res){
     
      var articleName = req.params.articleName; 
@@ -98,11 +105,7 @@ app.get('/:articleName', function(req,res){
 
 
 
-var counter = 0; 
-app.get('/counter' , function(req,res) {
-    counter = counter +1;
-    res.send(counter,toString());
-});
+
 
 
 
