f46498fc50479c383e277d5c8366a60ebc7fc7d9
Abhiram4572 <subramanyam4572@gmail.com>
2016-10-10 01:56:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,3 +21,19 @@ img.onclick = function() {
     var interval = setInterval(moveRight , 50);
     
 };
+
+
+//Counter code
+var counter = 0;
+var button = document.getElementById("counter");
+button.onclick = function() {
+//Make a request to counter endpoint 
+
+//Catch the response and store it in a variable
+
+
+//Render it in the correct span 
+counter = counter + 1;
+var span = document.getElementById("count");
+span.innerHTML = counter.toString();
+};
