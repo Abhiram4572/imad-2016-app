73afef3922eb5489f331726e10b276c9de59d63f
Abhiram4572 <subramanyam4572@gmail.com>
2016-10-09 15:31:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,19 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function(req,res){
+    res.send("ARTICLE ONE REQUESTED AND IS SERVED.");
+});
+
+app.get('/article-two', function(req,res){
+    res.send("ARTICLE TWO REQUESTED AND IS SERVED.");
+});
+
+app.get('/article-three', function(req,res){
+    res.send("ARTICLE THREE REQUESTED AND IS SERVED.");
+});
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
