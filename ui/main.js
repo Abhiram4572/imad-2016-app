ef78a38bb20c1610b33b06c9d48125108fbbeed5
Abhiram4572 <subramanyam4572@gmail.com>
2016-10-10 19:43:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,30 +1,32 @@
 
+//Move the image
+var img = document.getElementById("madi");
+
+var marginLeft = 0;
+
+function moveRight() {
+    marginLeft = marginLeft + 1 ;
+    img.style.marginLeft = marginLeft + 'px';
+}
+
+img.onclick = function() {
+    
+    var interval = setInterval(moveRight , 50);
+    
+};
+
+
+//Counter code
+var counter = 0;
 var button = document.getElementById("counter");
- var span = document.getElementById("count");
 button.onclick = function() {
-//Create request object
-var request = new XMLHttpRequest();
-request.open('GET', 'http://abhiram4572.imad.hasura-app.io/counter',true);
-request.send(null);
+//Make a request to counter endpoint 
 
 //Catch the response and store it in a variable
-request.onreadystatechange = function() {
-    
-        if(request.readystate === XMLHttpRequest.DONE){
-            
-            //Take some action
-            if(request.status === 200){
-                var counter = request.responseText;
-                span.innerHTML = counter.toString();
-                
-            }
-            
-            
-        }
- 
-    
-    };
-//Make a Request
 
 
+//Render it in the correct span 
+counter = counter + 1;
+var span = document.getElementById("count");
+span.innerHTML = counter.toString();
 };
