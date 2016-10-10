98777ddca213cede9f25aaa206c289481a625b7b
Abhiram4572 <subramanyam4572@gmail.com>
2016-10-10 23:00:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,8 +7,7 @@ var span = document.getElementById("count");
 button.onclick = function() {
 //Make a request to counter endpoint 
 var request = new XMLHttpRequest();
-request.open('GET' , 'http://abhiram4572.imad.hasura-app.io/counter' , true);
-request.send(null);
+
 //Catch the response and store it in a variable
 request.onreadystatechange = function(){
     
@@ -25,6 +24,9 @@ request.onreadystatechange = function(){
     
 };
 
+
+request.open('GET' , 'http://abhiram4572.imad.hasura-app.io/counter' , true);
+request.send(null);
 //Render it in the correct span 
 
 };
