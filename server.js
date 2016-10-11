4faa6542ea6305a8ca38bfc0e25b9930dfce4ff7
Abhiram4572 <subramanyam4572@gmail.com>
2016-10-11 19:27:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -119,7 +119,7 @@ var names = [];
 app.get('/submit-name/:name',function(req,res){
     var name = req.params.name;
     //JSON : Javascript Object Notation
-    names.push('name');
+    names.push(name);
     res.send(JSON.stringify(names));
 });
 
